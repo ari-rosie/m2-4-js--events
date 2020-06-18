cc1a53d6b9611bb8314e3d915cd1b33568f9bea6
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-18 14:41:33 -0400

    Exercice-1.1

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,24 @@
 // Exercise 1.1
 // ------------
 
+
+let body = document.querySelector('body');
+let p = document.querySelector('p');
+let flag = false;
+
+const userClick = function() {
+    flag = true;
+}
+
+const showMessage = function() {
+    body.removeEventListener('click', userClick);
+    if(flag)
+        p.innerHTML = 'You win! &#129321;';
+    else
+        p.innerHTML = 'You lose! &#128541;';
+}
+
+
+body.addEventListener('click', userClick);
+setTimeout(showMessage, 1000);
+
