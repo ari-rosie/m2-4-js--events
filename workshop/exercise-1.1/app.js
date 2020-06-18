47d9383c2c616ff0aadc67c8a0d8cd48ef8e1c45
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-18 12:28:05 -0400

    Exercice-1.0

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,11 @@
 // Exercise 1.1
 // ------------
 console.log('exercise 1.1');
+
+let body = document.querySelector('body');
+let p = document.querySelector('#result');
+const text = function(){
+    p.innerText = 'It works!';
+}
+body.addEventListener('click', text);
+
